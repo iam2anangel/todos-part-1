1ea52a600002b5e5b00f61752dbe922b2dbe7e5a
Jennifer Browning <browningjen@yahoo.com>
2018-11-19 12:58:55 -0500

    Updates

---
+++ precache-manifest.2fee0f10aa1f464fccd44f08048d9314.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ced8597f93d1e0b2f2c",
+    "url": "/todos-part-1/static/js/runtime~main.6ced8597.js"
+  },
+  {
+    "revision": "09526b4a1eea003e2e5a",
+    "url": "/todos-part-1/static/js/main.09526b4a.chunk.js"
+  },
+  {
+    "revision": "d855c10c80aa95827f2a",
+    "url": "/todos-part-1/static/js/1.d855c10c.chunk.js"
+  },
+  {
+    "revision": "09526b4a1eea003e2e5a",
+    "url": "/todos-part-1/static/css/main.c36dbe55.chunk.css"
+  },
+  {
+    "revision": "6d4884ffbe3ebfee75422bb76a550957",
+    "url": "/todos-part-1/index.html"
+  }
+];
